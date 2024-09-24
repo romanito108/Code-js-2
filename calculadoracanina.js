425ea18386a2475097749eb89c14e9e5c7359fc5
Diego Roman <77280127+romanito108@users.noreply.github.com>
2024-09-24 12:37:31 -0300

    Add files via upload

---
+++ calculadoracanina.js
@@ -0,0 +1,16 @@
+//idade do seu cachorro em anos de gente
+
+function calculadora() {
+let pergunta = prompt("Qual a idade do seu cachorrinho?")
+console.log(pergunta);
+
+
+let resultado = pergunta * 7
+
+alert ( "Seu cachorrinho tem "+ resultado + " anos em idade humana")
+
+}
+
+
+
+calculadora();
